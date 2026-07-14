437ab834398a1c0b431337839c24019ec3bf5a4d
shannpersand <shannpersand@gmail.com>
2026-07-13 21:41:31 -1000

    Use HashRouter and add GitHub Pages deploy workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Landing } from '@/pages/Landing';
 import { Lobby } from '@/pages/Lobby';
 import { Game } from '@/pages/Game';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/lobby/:code" element={<Lobby />} />
         <Route path="/game/:code" element={<Game />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
